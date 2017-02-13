4034bedd45d956afa17aa54d6a230fd9b1f94011
jploewen <jploewen@us.ibm.com>
2017-02-13 14:13:04 -0500

    update

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cors = require('cors');
 
 //Setup Cloudant Service.
 var appEnv = cfenv.getAppEnv();
-cloudantService = appEnv.getService("myMicroservicesCloudant");
+cloudantService = appEnv.getService("myDemoMicroservicesCloudant");
 var items = require('./routes/items');
 
 //Setup middleware.
